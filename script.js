c9fe1fe9cc06e4b1f03aa8521f55ac7870390176
adam-nour <adamnour4848@gmail.com>
2023-12-14 18:48:37 +0300

    added checkWinner and check for draw functionality

--- script.js
+++ script.js
@@ -1,37 +1,75 @@
-
-// In script.js
 let gameBoard = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-  ];
-  let turncontol = 1
-  function playerSelection(index) {
-    let row = Math.floor(index / 3);
-    let col = index % 3;
-  
-    
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', '']
+];
+let turnControl = 0;
 
-    
-    if (gameBoard[row][col] === '') {
+function playerSelection(index) {
+  let row = Math.floor(index / 3);
+  let col = index % 3;
 
-      if(turncontol%2==0){
-        gameBoard[row][col] = 'X'; // Assuming 'O' represents the computer's move
-        const button = document.getElementById(`button-${index}`);
-        button.innerHTML = '<h1>X</h1>';
-      }
-      else if(turncontol%2==1){
-        gameBoard[row][col] = 'O'; // Assuming 'X' represents the player's move
-        const button = document.getElementById(`button-${index}`);
-        button.innerHTML = '<h1>O</h1>';
-      }
-      turncontol++;
-      
-    } else {
-      alert('This cell is already selected. Please choose another.');
+  if (gameBoard[row][col] === '') {
+    if (turnControl % 2 === 0) {
+      gameBoard[row][col] = 'X';
+      const button = document.getElementById(`button-${index}`);
+      button.innerHTML = '<h1>X</h1>';
+    } else if (turnControl % 2 === 1) {
+      gameBoard[row][col] = 'O';
+      const button = document.getElementById(`button-${index}`);
+      button.innerHTML = '<h1>O</h1>';
     }
+    turnControl++;
     console.log(gameBoard);
-  
+
+    // Check for a winner or a draw after each move
+    if (checkWinner('X')) {
+      console.log('Player X wins!');
+    } else if (checkWinner('O')) {
+      console.log('Player O wins!');
+    } else if (isDraw()) {
+      console.log('It\'s a draw!');
+    }
+  } else {
+    alert('This cell is already selected. Please choose another.');
+  }
 }
 
+
+function checkWinner(player) {
+  // Check rows and columns for a winner
+  for (let i = 0; i < 3; i++) {
+    if (
+      (gameBoard[i][0] === player && gameBoard[i][1] === player && gameBoard[i][2] === player) ||
+      (gameBoard[0][i] === player && gameBoard[1][i] === player && gameBoard[2][i] === player)
+    ) {
+      return true;
+    }
+  }
+
+  // Check diagonals for a winner
+  if (
+    (gameBoard[0][0] === player && gameBoard[1][1] === player && gameBoard[2][2] === player) ||
+    (gameBoard[0][2] === player && gameBoard[1][1] === player && gameBoard[2][0] === player)
+  ) {
+    return true;
+  }
+
+  return false;
+}
+
+
+function isDraw() {
+  // Check for empty spots on the board
+  for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 3; col++) {
+      if (gameBoard[row][col] === '') {
+        return false; 
+      }
+    }
+  }
+
   
+  return !checkWinner('X') && !checkWinner('O');
+}
+
